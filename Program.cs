6dd34649c8a0cf95fe0931ee272811ae5c0d7085
DannyTittel <146334880+DannyTittel@users.noreply.github.com>
2024-03-18 10:04:49 +0100

    Tekst update

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
     public static void Main()
     {
-        RestaurantInfo restaurant = new RestaurantInfo("Wijnhaven 107","3011WN Rotterdam","0612316367","09:00/20:00, Mon/Sat");
+        RestaurantInfo restaurant = new RestaurantInfo("Wijnhaven 107","3011 WN Rotterdam","+ 31 612316367","09:00/20:00, Mon/Sat");
         System.Console.WriteLine(restaurant.Info());
         var manager = new Reserveringen();
 
