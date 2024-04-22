ed81ad75e26a3441e26c85292947ff39dc1becd0
Yunus <1080493@hr.nl>
2024-04-22 09:53:12 +0200

    UnitTest voegreserveringtoe false

--- ProjectBTest/UnitTest1.cs
+++ ProjectBTest/UnitTest1.cs
@@ -15,18 +15,29 @@ namespace ProjectBTest
         }
 
         [TestMethod]
-        public void VoegReserveringToeTest()
+        public void VoegReserveringToe_True()
         {
             string guestName = "John Doe";
             int numberOfPeople = 4;
-            DateTime reservationDateTime = DateTime.Now.AddDays(1);
+            DateTime reservationDateTime = DateTime.Now.AddHours(5);
             string note = "Vegetarian";
 
             bool result = _reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime, note);
 
             Assert.IsTrue(result);
         }
-    }
 
-        
+        [TestMethod]
+        public void VoegReserveringToe_False()
+        {
+            string guestName = "Jane Doe";
+            int numberOfPeople = 2;
+            DateTime reservationDateTime = DateTime.Now.AddHours(-5);
+            string note = "Gluten Free";
+            
+            bool result = _reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime, note);
+
+            Assert.IsFalse(result);
+        }
+    }
 }
