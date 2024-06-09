0cd323319861b3e6201346469fe606bcaaa264d4
proudpimp <146075366+proudpimp@users.noreply.github.com>
2024-06-09 15:32:42 +0200

    Unittest maken/verbeteren

--- ProjectBTest/UnitTest1.cs
+++ ProjectBTest/UnitTest1.cs
@@ -1,76 +1,96 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
-// namespace ProjectBTest
-// {
-//     [TestClass]
-//     public class ProjectBTest
-//     {
-//         [TestMethod]
-//         public void VoegReserveringToe_True()
-//         {
-//             string guestName = "John Doe";
-//             int numberOfPeople = 4;
-//             DateTime reservationDateTime = DateTime.Now.AddDays(1);
-//             string note = "Vegetarian";
-//             string tcode = "4A";
+namespace ProjectBTest
+{
+    [TestClass]
+    public class ProjectBTest
+    {
+        [TestMethod]
+        public void VoegReserveringToe_True()
+        {
+            string guestName = "John Doe";
+            int numberOfPeople = 4;
+            DateTime reservationDateTime = DateTime.Now.AddDays(1);
+            string note = "Vegetarian";
+            string tcode = "4A";
+            int safetynum = new Random().Next(1000,10000);
+            bool result = Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note,safetynum);
 
-//             bool result = Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note);
+            Assert.IsTrue(result);
+        }
 
-//             Assert.IsTrue(result);
-//         }
+        [TestMethod]
+        public void VoegReserveringToe_False()
+        {
+            string guestName = "Jane Doe";
+            int numberOfPeople = 2;
+            DateTime reservationDateTime = DateTime.Now;
+            string note = "Gluten Free";
+            string tcode = "2B";
+            int safetynum = new Random().Next(1000,10000);
 
-//         [TestMethod]
-//         public void VoegReserveringToe_False()
-//         {
-//             string guestName = "Jane Doe";
-//             int numberOfPeople = 2;
-//             DateTime reservationDateTime = DateTime.Now;
-//             string note = "Gluten Free";
-//             string tcode = "2B";
             
-//             bool result = Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note);
+            bool result = Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note,safetynum);
 
-//             Assert.IsFalse(result);
-//         }
-//         [TestMethod]
-//         public void CancelReservation_True()
-//         {
-//             string guestName = "John Doe";
-//             int numberOfPeople = 4;
-//             DateTime reservationDateTime = DateTime.Now;
-//             string note = "Vegan";
-//             string tcode = "4B";
-//             Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void CancelReservation_True()
+        {
+            string guestName = "John Doe";
+            int numberOfPeople = 4;
+            DateTime reservationDateTime = DateTime.Now.AddMinutes(1);
+            string note = "Vegan";
+            string tcode = "4B";
+            int safetynum = new Random().Next(1000,10000);
+            Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note,safetynum);
 
-//             var result = Reserveringen.AnnuleerReservering(guestName);
-//             Assert.IsTrue(result);
-//         }
-//         [TestMethod]
-//         public void CancelReservation_False()
-//         {
-//             string guestName = "John Doe";
-//             int numberOfPeople = 4;
-//             DateTime reservationDateTime = DateTime.Now;
-//             string note = "Vegan";
-//             string tcode = "4B";
-//             Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note);
-//             string fakeNaam = "Rotterdam";
+            var result = Reserveringen.AnnuleerReservering(guestName,safetynum);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void CancelReservation_False()
+        {
+            string guestName = "John Doe";
+            int numberOfPeople = 4;
+            DateTime reservationDateTime = DateTime.Now.AddMinutes(1);
+            string note = "Vegan";
+            string tcode = "4B";
+            int safetynum = new Random().Next(1000,10000);
+            Reserveringen.VoegReserveringToe(guestName, numberOfPeople, reservationDateTime,tcode, note,safetynum);
+            string fakeNaam = "Rotterdam";
 
-//             var result = Reserveringen.AnnuleerReservering(fakeNaam);
-//             Assert.IsFalse(result);
-//         }
-//         [TestMethod]
-//         public void MakeAccount_True()
-//         {
-//             string name = "John Doe";
-//             string email = "jdoe@gmail.com";
-//             string password = "JohnDoe1";
-//             DateTime birthday = DateTime.Now.AddYears(-5);
-//             string postcode = "1212ZB";
-//             string phoneNumber = "0612345678";
-//             var result = Account.VoegAccountToe(name,email,password,birthday,postcode,phoneNumber);
-//             Assert.IsTrue(result);
-//         }
-//     }
-// }
+            var result = Reserveringen.AnnuleerReservering(fakeNaam,safetynum);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void MakeAccount_True()
+        {
+            string name = "John Doe";
+            string email = "jdoe@gmail.com";
+            string password = "JohnDoe1";
+            DateTime birthday = DateTime.Now.AddYears(-5);
+            string postcode = "1212ZB";
+            string phoneNumber = "0612345678";
+            int verificationNum = new Random().Next(1000,10000);
+
+            var result = Account.VoegAccountToe(name,email,password,birthday,postcode,phoneNumber,verificationNum);
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void MakeAccount_False()
+        {
+            string name = "";
+            string email = "jdoe@gmail.com";
+            string password = "JohnDoe1";
+            DateTime birthday = DateTime.Now.AddYears(-5);
+            string postcode = "1212ZB";
+            string phoneNumber = "1612345678";
+            int verificationNum = new Random().Next(1000,10000);
+
+            var result = Account.VoegAccountToe(name,email,password,birthday,postcode,phoneNumber,verificationNum);
+            Assert.IsFalse(result);
+        }
+    }
+}
