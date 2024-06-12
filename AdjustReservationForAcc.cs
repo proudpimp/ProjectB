e9043857acad55f01574a3c6ba4eaea1fd980fba
1018045 <1018045@hr.nl>
2024-06-12 12:16:33 +0200

    AdjustforAcc afgemaakt
    Je kan nu kiezen welke reservering je wilt adjusten.

--- AdjustReservationForAcc.cs
+++ AdjustReservationForAcc.cs
@@ -2,8 +2,65 @@ public class AdjustReservationforAcc : IAdjust
 {
     public void Adjust()
     {
-        AccountReservations.View();
-        Console.WriteLine("Which reservation would you like to adjust? ");
+         string email = Login.CurrentUserEmail;
+        var reserveringen = Reserveringen.GetReservationByEmail(email);
+        if (reserveringen != null)
+        {
+            foreach(var reservering in reserveringen)
+            {
+                System.Console.WriteLine("Your current reservation details:");
+                System.Console.WriteLine("-------------------------------------");
+                System.Console.WriteLine($"Email: {reservering.Email}");
+                System.Console.WriteLine($"Name: {reservering.GastNaam}");
+                System.Console.WriteLine($"Number of People: {reservering.AantalPersonen}");
+                System.Console.WriteLine($"Date and Time: {reservering.DatumTijd.ToString("yyyy-MM-dd HH:mm")}");
+                System.Console.WriteLine($"Notes: {reservering.Notitie}");
+                System.Console.WriteLine($"Tablecode: {reservering.TableCode}");
+                System.Console.WriteLine("-------------------------------------");
+
+            
+
+
+            }
+
+            Console.WriteLine("Which reservation would you like to adjust?");
+            Console.Write("Fill in the date and time of the reservation you want to adjust (yyyy-mm-dd hh:mm): ");
+            DateTime userdatum;
+            while (!DateTime.TryParse(Console.ReadLine(), out userdatum) || userdatum < DateTime.Now)
+            {
+                Console.WriteLine("Invalid date. Please enter a future date and time in the format yyyy-mm-dd hh:mm:");
+            }
+
+            var reserveringToAdjust = reserveringen.FirstOrDefault(r => r.DatumTijd == userdatum);
+            if (reserveringToAdjust == null)
+            {
+                Console.WriteLine("No reservation found with the specified date and time.");
+                return;
+            }
+
+            Console.Write("Enter the new date and time (yyyy-mm-dd hh:mm): ");
+            DateTime newDatumTijd;
+            while (!DateTime.TryParse(Console.ReadLine(), out newDatumTijd) || newDatumTijd < DateTime.Now)
+            {
+                Console.WriteLine("Invalid date. Please enter a future date and time in the format yyyy-mm-dd hh:mm:");
+            }
+
+            reserveringToAdjust.DatumTijd = newDatumTijd;
+            Reserveringen.SaveReservationsToJsonForAcc();
+        
+
+            Console.WriteLine("Reservation updated successfully.");
+        }
+        else
+        {
+            Console.WriteLine("No reservations found for the current user.");
+        }
+        
+
+        
+
+
         
     }
 }
+
